d1a400dc182f31e5b52d4e3a73fa42c082271f92
mohinderps <mohin19derp@gmail.com>
2021-05-31 15:41:09 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import house1 from "./images/desktop/house-1.jpeg";
+import house2 from "./images/desktop/house-2.jpeg";
+import house3 from "./images/desktop/house-3.jpeg";
+import house4 from "./images/desktop/house-4.jpeg";
+
+console.log(house1);
+console.log(house2);
+console.log(house3);
+console.log(house4);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={house1} alt="house 1" />
+        <img src={house2} alt="house 2" />
+        <img src={house3} alt="house 3" />
+        <img src={house4} alt="house 4" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
